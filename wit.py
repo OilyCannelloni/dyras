f8bb60ae214f32c81a3a414e9da3057fdafa36c5
Bartek Slupik <bartek.slupik@gmail.com>
2019-01-13 23:52:05 +0100

    Update wit.py

--- wit.py
+++ wit.py
@@ -1,19 +1,15 @@
 n = int(input())
-p = {}
-strings = {'A', 'B', 'C', 'AB', 'BC', 'AC', 'ABC'}
-for key in strings:
-    p[key] = 999999
-
+p = {'A': 999999, 'B': 999999, 'C': 999999, 'AB': 999999, 'BC': 999999, 'AC': 999999, 'ABC': 999999}
 for i in range(n):
     inp = input().split(" ")
     inp[1] = ''.join(sorted(list(inp[1])))
     inp[1] += 'AC' if inp[1] == 'ABC' else ''
-    for el in strings:
-        p[el] = int(inp[0]) if el in inp[1] and int(inp[0]) < p[el] else p[el]
-
+    for el in p.keys():
+        p[el] = (int(inp[0]) if el in inp[1] and int(inp[0]) < p[el] else p[el]) 
+        
 for el in ['A', 'B', 'C']:
     p[el] = min({k: v for k, v in p.items() if el in k}.values())
-
+    
 if 999999 in (p[el] for el in ['A', 'B', 'C']):
     print(-1)
 else:
