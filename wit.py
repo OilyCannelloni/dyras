3de724d126b78841c3ca9c63e5e7de9b8ba6feab
Bartek Slupik <bartek.slupik@gmail.com>
2019-01-16 18:03:19 +0100

    Update wit.py

--- wit.py
+++ wit.py
@@ -1,16 +1,13 @@
-n = int(input())
 p = {'A': 999999, 'B': 999999, 'C': 999999, 'AB': 999999, 'BC': 999999, 'AC': 999999, 'ABC': 999999}
-for i in range(n):
+for i in range(int(input())):
     inp = input().split(" ")
     inp[1] = ''.join(sorted(list(inp[1])))
     inp[1] += 'AC' if inp[1] == 'ABC' else ''
     for el in p.keys():
-        p[el] = (int(inp[0]) if el in inp[1] and int(inp[0]) < p[el] else p[el]) 
+        p[el] = (int(inp[0]) if el in inp[1] and int(inp[0]) < p[el] else p[el])
         
 for el in ['A', 'B', 'C']:
     p[el] = min({k: v for k, v in p.items() if el in k}.values())
     
-if 999999 in (p[el] for el in ['A', 'B', 'C']):
-    print(-1)
-else:
-    print(min([p['A'] + p['B'] + p['C'], p['A'] + p['BC'], p['B'] + p['AC'], p['C'] + p['AB'], p['ABC']]))
+t = min([p['A'] + p['B'] + p['C'], p['A'] + p['BC'], p['B'] + p['AC'], p['C'] + p['AB'], p['ABC']])
+print(-1 if 999999 in (p[el] for el in ['A', 'B', 'C']) else t)
