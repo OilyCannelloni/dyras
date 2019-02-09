9e32c087c9db2c733acb183168fe5b84e314bd4a
Bartek Slupik <bartek.slupik@gmail.com>
2019-02-09 21:02:44 +0100

    Update nau.py

--- nau.py
+++ nau.py
@@ -1,6 +1,4 @@
-ls = list([int(el) for el in input().split(" ")])
-res = []
-for i in [0, 2, 4]:
-    res.append(ls[i]/ls[i+1])
-ind = res.index(max(res))
-print(ls[2*ind], ls[2*ind+1], end=" ")
+ls = [int(el) for el in input().split(" ")]             # Put input numbers into a list
+res = [ls[i]/ls[i+1] for i in range(0, len(ls), 2)]     # Create a new list with division results
+ind = res.index(max(res))                               # Get the index of the highest quotient
+print(ls[2*ind], ls[2*ind+1])                           # Print the appropriate fraction
