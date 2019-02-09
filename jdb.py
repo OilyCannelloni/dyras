88371b31b2d2026e2fe5ce666f8de6d7dcaf9ac0
Bartek Slupik <bartek.slupik@gmail.com>
2019-02-09 20:39:41 +0100

    Update jdb.py

--- jdb.py
+++ jdb.py
@@ -1,4 +1,4 @@
-lengths = list(len(el) for el in input().split(" "))
-for i in range(1, 13):
-    if i not in lengths:
-        print(i, end=" ")
+lengths = [len(el) for el in input().split(" ")]    # Get word lengths
+for i in range(1, 13):  # For every length from 1 to 12 inclusive
+    if i not in lengths:    # If it hasn't been used
+        print(i, end=" ")   # Print it with a space instead of '\n'
